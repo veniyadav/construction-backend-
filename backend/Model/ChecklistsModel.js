20eb52607d4ade9cf0836bc5f25c68526bac3b8d
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-02 19:19:38 +0530

    Update ChecklistsModel.js

--- backend/Model/ChecklistsModel.js
+++ backend/Model/ChecklistsModel.js
@@ -2,8 +2,14 @@ const mongoose =require("mongoose")
 
 const ChecklistsSchema = new mongoose.Schema({
     checklistName: { type: String, required: true },
-    project: { type: String, required: true },
-    AssignTo: { type: String, required: true },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Projects", // Replace with your actual category model name
+      required: true, },
+    AssignTo: { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,},
     date: { type: Date, required: true },
   
     checklistItems: [
