9e44570889e0194becfa831b5f8fb6d39235317f
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-21 17:14:01 +0530

    Create HazardModel.js

---
+++ backend/Model/HazardModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const HazardSchema = new mongoose.Schema(
+  {
+    hazardDescription: { type: String, required: true },
+    severityLevel: {
+      type: String,
+      required: true,
+    },
+    likelihood: {
+      type: String,
+     // required: true,
+    },
+     
+    impact: {
+      type: String,
+      //required: true,
+    },
+
+    riskMatrix: {
+      type: String,
+     // required: true, // e.g., "3 x 2"
+    },
+
+    
+    additionalNotes: { type: String, required: true },
+    responsiblePerson: { type: String, required: true },
+    controlVerification: {
+      type: String,
+      
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+
+module.exports = mongoose.model("Hazard", HazardSchema);
