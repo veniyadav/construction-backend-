ba8e0b427970db500c4530b7bbf94a36e30bf9f0
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-21 16:56:04 +0530

    Update DocumentsModel.js

--- backend/Model/DocumentsModel.js
+++ backend/Model/DocumentsModel.js
@@ -1,12 +1,11 @@
 const mongoose =require("mongoose")
 
 const DocumentsSchema = new mongoose.Schema({
-    folder: { type: String, required: true },
+    
     documentName: { type: String, required: true },
     documentType: { type: String, required: true },
     assignTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     dueDate: { type: Date, required: true },
-    submissionDate: { type: Date, required: true },
     status: { type: String, required: true },
     comments: { type: String, required: true },
     image: [],
