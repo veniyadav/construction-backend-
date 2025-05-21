ec5d69b13c52cf66cf266359875c133d44d99fe3
Veni Prasad Yadav <120805086+veniyadav@users.noreply.github.com>
2025-05-21 16:58:58 +0530

    Update announcementModel.js

--- backend/Model/announcementModel.js
+++ backend/Model/announcementModel.js
@@ -18,29 +18,31 @@ const announcementSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+
   message: {
     type: String,
     required: true,  // Message is required
   },
+
   image: [],
 
-   groups: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to User model
-    required: true,
-    default: [],
-  }],
-  // `individuals` also refers to `User` model (User ObjectId)
-  individuals: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Reference to User model
-    required: true,
-    default: [],
-  }]
+  // groups: [{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User', // Reference to User model
+  //   required: true,
+  //   default: [],
+  // }],
   
+  // // individuals also refers to User model (User ObjectId)
+  // individuals: [{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User', // Reference to User model
+  //   required: true,
+  //   default: [],
+  // }]
   
 }, { timestamps: true });  // Adds createdAt and updatedAt timestamps
 
 const Announcement = mongoose.model('Announcement', announcementSchema);
 
-module.exports = Announcement;
+module.exports = Announcement; 
